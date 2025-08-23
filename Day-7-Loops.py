ad16266f5e87a416b1cea8fa874cdb206e71e45f
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-08-23 23:26:22 +0530

    Day-7Loops

---
+++ Day-7-Loops.py
@@ -0,0 +1,24 @@
+n = int(input())
+for i in range(0, n):
+    print(i)
+
+
+li = ["geeks", "for", "geeks"]
+for i in li:
+    print(i)
+    
+tup = ("geeks", "for", "geeks")
+for i in tup:
+    print(i)
+    
+s = "Geeks"
+for i in s:
+    print(i)
+    
+d = dict({'x':123, 'y':354})
+for i in d:
+    print(i, d[i])
+    
+set1 = {1, 2, 3, 4, 5, 6}
+for i in set1:
+    print(i),
