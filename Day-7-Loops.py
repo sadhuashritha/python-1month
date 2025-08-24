763b43133be11cdd80f09dbd2f9c34e1f3ab2faf
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-08-24 23:11:25 +0530

    upadted Day-7Loops

--- Day-7-Loops.py
+++ Day-7-Loops.py
@@ -22,3 +22,35 @@ for i in d:
 set1 = {1, 2, 3, 4, 5, 6}
 for i in set1:
     print(i),
+
+#You are given a number N, you need to print its multiplication table.
+N =int(input())
+for i in range(1,11):
+    print(N * i, end=" ")
+    
+
+#You are given a number n, take input of n and print its multiplication table in a single line using for loop till n * 10. 
+n = int(input())
+for i in range (1,11):
+    print(n * i, end = " ")
+
+#You are given a string s, you need to print its characters at even indices(index starts at 0).
+s = str(input())
+for i in range(0,len(s),2):
+        print(s[i], end="")
+
+#Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
+x = int(input())
+while x >= 0:
+    print(x, end= " ")
+    x -= 1
+
+#Given a positive integer x, the task is to print the numbers from 1 to x in the order as 1^2, 2^2, 3^2, 4^2, 5^2, ... (in increasing order).
+x = int(input())
+i = 1
+while(x >= i**2):
+    print (i**2 , end = " ")
+    i+=1
+        
+
+
