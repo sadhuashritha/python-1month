8969f68dbf20892f1421ead8f7103607c1b32679
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-08-25 22:04:13 +0530

    updated

--- Day-7-Loops.py
+++ Day-7-Loops.py
@@ -1,19 +1,19 @@
 n = int(input())
 for i in range(0, n):
-    print(i)
+    print(i,end="")
 
 
 li = ["geeks", "for", "geeks"]
 for i in li:
-    print(i)
+    print(i,end="")
     
 tup = ("geeks", "for", "geeks")
 for i in tup:
-    print(i)
+    print(i,end=" ")
     
 s = "Geeks"
 for i in s:
-    print(i)
+    print(i,end=" ")
     
 d = dict({'x':123, 'y':354})
 for i in d:
@@ -21,7 +21,7 @@ for i in d:
     
 set1 = {1, 2, 3, 4, 5, 6}
 for i in set1:
-    print(i),
+    print(i,end=" "),
 
 #You are given a number N, you need to print its multiplication table.
 N =int(input())
@@ -52,5 +52,47 @@ while(x >= i**2):
     print (i**2 , end = " ")
     i+=1
         
+#Given an integer,perform the following conditional actions:
 
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format:
+# A single line containing a positive integer, .
 
+# Output Format:
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+n = int(input().strip())
+if n % 2 == 0 and n in range(2,6):
+    print("Not Weird")
+elif n % 2 == 0 and n in range(6,21):
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
+else:
+    print("Weird")
+
+#For all non-negative integers , i < n print i ^2
+# Example
+# The list of non-negative integers that are less than n = 3 is [0,1,2]. Print the square of each number on a separate line.
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
+
+#You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+# For Example:
+# Www.HackerRank.com → wWW.hACKERrANK.COM
+# Pythonist 2 → pYTHONIST 2  
+s = str(input())
+res = ""
+for i in s:
+    if i.islower():
+        res += i.upper()
+    elif i.isupper():
+        res += i.lower()
+    else:
+        res += i
+
+print(res)
+    
