bcf49b43819570d000467c8b6b20bc98981f6e4e
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-02 22:38:17 +0530

    updated

--- Day-7-Loops.py
+++ Day-7-Loops.py
@@ -1,4 +1,4 @@
-# n = int(input())
+# n = int(input("Enter a number"))
 # for i in range(0, n):
 #     print(i,end="")
 
@@ -30,23 +30,23 @@
     
 
 # #You are given a number n, take input of n and print its multiplication table in a single line using for loop till n * 10. 
-# n = int(input())
+# n = int(input("Enter a number"))
 # for i in range (1,11):
 #     print(n * i, end = " ")
 
 # #You are given a string s, you need to print its characters at even indices(index starts at 0).
-# s = str(input())
+# s = str(input("Enter a string"))
 # for i in range(0,len(s),2):
 #         print(s[i], end="")
 
 # #Given a number x, the task is to print the numbers from x to 0 in decreasing order in a single line.
-# x = int(input())
+# x = int(input("Enter a number"))
 # while x >= 0:
 #     print(x, end= " ")
 #     x -= 1
 
 # #Given a positive integer x, the task is to print the numbers from 1 to x in the order as 1^2, 2^2, 3^2, 4^2, 5^2, ... (in increasing order).
-# x = int(input())
+# x = int(input("Enter a number"))
 # i = 1
 # while(x >= i**2):
 #     print (i**2 , end = " ")
@@ -63,7 +63,7 @@
 
 # # Output Format:
 # # Print Weird if the number is weird. Otherwise, print Not Weird.
-# n = int(input().strip())
+# n = int(input("Enter a number").strip())
 # if n % 2 == 0 and n in range(2,6):
 #     print("Not Weird")
 # elif n % 2 == 0 and n in range(6,21):
@@ -76,7 +76,7 @@
 # #For all non-negative integers , i < n print i ^2
 # # Example
 # # The list of non-negative integers that are less than n = 3 is [0,1,2]. Print the square of each number on a separate line.
-#     n = int(input())
+#     n = int(input("Enter a number"))
 #     for i in range(0,n):
 #         print(i**2)
 
@@ -84,7 +84,7 @@
 # # For Example:
 # # Www.HackerRank.com → wWW.hACKERrANK.COM
 # # Pythonist 2 → pYTHONIST 2  
-# s = str(input())
+# s = str(input("Enter a string"))
 # res = ""
 # for i in s:
 #     if i.islower():
@@ -110,23 +110,45 @@
 #                 print(True)
 # print(False)
 
-#Write a function that reverses a string. The input string is given as an array of characters s.
+# # Given an integer n, return true if it is a power of three. Otherwise, return false.
 
-# You must do this by modifying the input array in-place with O(1) extra memory.4444
+# # An integer n is a power of three, if there exists an integer x such that n == 3x.
+# n = int(input("Enter a number: "))
+# if n <= 0:
+#     print(False) 
+# for i in range(31):
+#     if 3**i == n:
+#         print(True)
+# print(False)
 
-s = list(input("Enter a string(reverse a string)"))
-l = 0
-r = len(s) - 1
-while l < r:
-    s[l],s[r] = s[r],s[l]
-    l+=1
-    r-=1
-s = "".join(s)
-print(s)
+# # Write a function that reverses a string. The input string is given as an array of characters s.
 
-#You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
-line = "This is a string"
-line = line.split(" ")
-print(line)
-line = "-".join(line)
-print(line) 
+# # You must do this by modifying the input array in-place with O(1) extra memory.4444
+
+# s = list(input("Enter a string(reverse a string)"))
+# l = 0
+# r = len(s) - 1
+# while l < r:
+#     s[l],s[r] = s[r],s[l]
+#     l+=1
+#     r-=1
+# s = "".join(s)
+# print(s)
+
+# #You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+# line = "This is a string"
+# line = line.split(" ")
+# print(line)
+# line = "-".join(line)
+# print(line) 
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+nums = list(map(int,input("Enter a array").split()))
+nums.sort()
+n=len(nums)
+for i in range(0,n-1,2):
+    if(nums[i]!=nums[i+1]):
+        print(nums[i]) 
+        
+else:
+    print(nums[n-1])
