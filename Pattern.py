dd29790af555292572f1e09244eb323fb0b12d74
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-05 20:33:32 +0530

    updated pattern

--- Pattern.py
+++ Pattern.py
@@ -1,148 +1,167 @@
-#Input: n = 5
-# Output:
-# *
-# **
-# ***
-# ****
-# *****
+# #Input: n = 5
+# # Output:
+# # *
+# # **
+# # ***
+# # ****
+# # *****
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+# print()
+
+# # Input: n = 5
+# # Output:
+# # *****
+# # ****
+# # ***
+# # **
+# # *
+
+# n = 6
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# print()
+
+# # Input: n = 5
+# # Output:
+# #     *
+# #    ***
+# #   *****
+# #  *******
+# # *********
+
+# n = 6
+# sum = 0
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for k in range((2*i) - 1):
+#         print("*",end=" ")
+#     print()
+
+# print()
+
+# # Input: n = 5
+# # Output:
+# # 1
+# # 12
+# # 123
+# # 1234
+# # 12345
+
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# print()
+
+# # n=5
+# #         * 
+# #       * * *
+# #     * * * * *
+# #   * * * * * * * 
+# # * * * * * * * * *
+# #   * * * * * * * 
+# #     * * * * *
+# #       * * *
+# #         *
+
+# n = 5
+# for i in range(1,n+1):
+#     for j in range(n - i):
+#         print(" ",end=" ")
+#     for k in range((2*i) - 1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(n-1,0,-1):
+#     for j in range(n - i):
+#         print(" ", end=" ")
+#     for k in range((2*i) - 1):
+#         print("*",end=" ")
+#     print()
+
+
+# print() 
+
+# # Input: n = 5
+# # Output:
+# #     *
+# #    ***
+# #   *****
+# #    ***
+# #     *
+
+# n = 5
+# mid = (n + 1)//2
+# for i in range(1,mid+1):
+#     for j in range(mid - i):
+#         print(" ",end=" ")
+#     for k in range((2*i) - 1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(mid-1,0,-1):
+#     for j in range(mid - i):
+#         print(" ",end=" ")
+#     for k in range((2*i) -1):
+#         print("*",end=" ")
+#     print()
+
+
+# print()
+
+# # n=5
+# # *   *
+# #  * * 
+# #   *  
+# #  * * 
+# # *   *
+# n = 5
+# mid = (n+1)//2
+# for i in range(1,mid+1):
+#     for j in range(1,n+1):
+#         if j == i or j == (n-i) + 1:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+# for i in range(mid-1,0,-1):
+#     for j in range(1,n+1):
+#         if j==i or j == (n-i) + 1:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+# print()
+
 n = 5
-for i in range(1,n+1):
-    for j in range(i):
-        print("*", end=" ")
+alpha = 65
+for i in range(0,n):
+    print(" " * (n-i), end=" ")
+    for j in range(0,i+1):
+        print(chr(alpha),end=" ")
+        alpha+=1
+    alpha = 65
     print()
 
-print()
-
-# Input: n = 5
-# Output:
-# *****
-# ****
-# ***
-# **
-# *
-
-n = 6
-for i in range(n,0,-1):
-    for j in range(i):
-        print("*",end=" ")
-    print()
-
-print()
-
-# Input: n = 5
-# Output:
-#     *
-#    ***
-#   *****
-#  *******
-# *********
-
-n = 6
-sum = 0
-for i in range(1,n+1):
-    for j in range(n-i):
-        print(" ",end=" ")
-    for k in range((2*i) - 1):
-        print("*",end=" ")
-    print()
-
-print()
-
-# Input: n = 5
-# Output:
-# 1
-# 12
-# 123
-# 1234
-# 12345
-
-n = 5
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
-
-print()
-
-# n=5
-#         * 
-#       * * *
-#     * * * * *
-#   * * * * * * * 
-# * * * * * * * * *
-#   * * * * * * * 
-#     * * * * *
-#       * * *
-#         *
-
-n = 5
-for i in range(1,n+1):
-    for j in range(n - i):
-        print(" ",end=" ")
-    for k in range((2*i) - 1):
-        print("*",end=" ")
-    print()
-
-for i in range(n-1,0,-1):
-    for j in range(n - i):
-        print(" ", end=" ")
-    for k in range((2*i) - 1):
-        print("*",end=" ")
-    print()
-
-
-print() 
-
-# Input: n = 5
-# Output:
-#     *
-#    ***
-#   *****
-#    ***
-#     *
-
-n = 5
-mid = (n + 1)//2
-for i in range(1,mid+1):
-    for j in range(mid - i):
-        print(" ",end=" ")
-    for k in range((2*i) - 1):
-        print("*",end=" ")
-    print()
-
-for i in range(mid-1,0,-1):
-    for j in range(mid - i):
-        print(" ",end=" ")
-    for k in range((2*i) -1):
-        print("*",end=" ")
-    print()
-
-
-print()
-
-# n=5
-# *   *
-#  * * 
-#   *  
-#  * * 
-# *   *
-n = 5
-mid = (n+1)//2
-for i in range(1,mid+1):
-    for j in range(1,n+1):
-        if j == i or j == (n-i) + 1:
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
-
-for i in range(mid-1,0,-1):
-    for j in range(1,n+1):
-        if j==i or j == (n-i) + 1:
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
-
-print()
-
+# n = 5
+# alph = 65
+# for i in range(0, n):
+#     print(" " * (n-i), end=" ")
+#     for j in range(0, i+1):
+#         print(chr(alph), end=" ")
+#         alph += 1
+#     alph = 65
+#     print()
