02ca6572e9a32b94aeade5d9912f315e2f8da4ba
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-05 19:28:40 +0530

    updated pattern

--- Day-1-Variables.py
+++ Day-1-Variables.py
@@ -11,3 +11,59 @@ print(a)
 
 a =True
 print(type(a))
+
+#Declare two integer variables and print their sum.
+a = 3
+b = 5
+print(a + b)
+
+#Assign your name to a variable and print it.
+z = "Ashritha"
+print(z)
+
+#Store an integer in a variable, then change its value to a float and print both.
+z = 6
+y = float(z)
+print(z,y)
+
+#Assign the same value to three variables in one line and print them.
+m=n=o = 17
+print(m,n,o)
+
+#Swap two variables without using a third variable.
+s = 5
+p = 9
+s,p = p,s
+print(s,p)
+
+#Assign multiple values to multiple variables in one line and print them.
+s,p,q = 3,5,6
+print(s,p,q)
+
+#Store your age in a variable and print "I am X years old" using that variable.
+age = 20
+print("I am ",age,"years old")
+
+#Assign a boolean value (True/False) to a variable and print it.
+v = False
+print(v)
+print(type(v))
+
+#Concatenate two string variables and print the result.
+x = "Hello"
+y = "World"
+print(x + " " + y)
+
+#Store a number in a variable and print its square using that variable.
+d = 5
+print(d*d)
+
+#Assign a decimal number to a variable and convert it into an integer.
+f = 5.6
+g = int(f)
+print(g)
+
+#Declare a variable, delete it using del, and try printing it.
+k = 40
+del k
+#print(k)
--- Pattern.py
+++ Pattern.py
@@ -1,14 +1,41 @@
+#Input: n = 5
+# Output:
+# *
+# **
+# ***
+# ****
+# *****
 n = 5
 for i in range(1,n+1):
     for j in range(i):
         print("*", end=" ")
     print()
 
+print()
+
+# Input: n = 5
+# Output:
+# *****
+# ****
+# ***
+# **
+# *
+
 n = 6
 for i in range(n,0,-1):
     for j in range(i):
         print("*",end=" ")
     print()
+
+print()
+
+# Input: n = 5
+# Output:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
 
 n = 6
 sum = 0
@@ -19,12 +46,34 @@ for i in range(1,n+1):
         print("*",end=" ")
     print()
 
+print()
+
+# Input: n = 5
+# Output:
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
 n = 5
 for i in range(1,n+1):
     for j in range(1,i+1):
         print(j,end=" ")
     print()
 
+print()
+
+# n=5
+#         * 
+#       * * *
+#     * * * * *
+#   * * * * * * * 
+# * * * * * * * * *
+#   * * * * * * * 
+#     * * * * *
+#       * * *
+#         *
 
 n = 5
 for i in range(1,n+1):
@@ -41,6 +90,17 @@ for i in range(n-1,0,-1):
         print("*",end=" ")
     print()
 
+
+print() 
+
+# Input: n = 5
+# Output:
+#     *
+#    ***
+#   *****
+#    ***
+#     *
+
 n = 5
 mid = (n + 1)//2
 for i in range(1,mid+1):
@@ -56,3 +116,33 @@ for i in range(mid-1,0,-1):
     for k in range((2*i) -1):
         print("*",end=" ")
     print()
+
+
+print()
+
+# n=5
+# *   *
+#  * * 
+#   *  
+#  * * 
+# *   *
+n = 5
+mid = (n+1)//2
+for i in range(1,mid+1):
+    for j in range(1,n+1):
+        if j == i or j == (n-i) + 1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+for i in range(mid-1,0,-1):
+    for j in range(1,n+1):
+        if j==i or j == (n-i) + 1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print()
+
