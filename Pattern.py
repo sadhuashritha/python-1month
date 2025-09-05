1accc52578222240335aa02dce04dae25a4c3541
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-05 22:21:56 +0530

    pattern updated

--- Pattern.py
+++ Pattern.py
@@ -165,3 +165,18 @@ for i in range(0,n):
     print()
 
 
+# n = 5
+#         *
+#       *   *
+#     *       *
+#   *           *
+# * * * * * * * * *
+n = 5
+for i in range(1,n+1):
+    for j in range(1,(2*n)):
+        if i == n or j == (n-i) + 1 or j == n + (i - 1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
