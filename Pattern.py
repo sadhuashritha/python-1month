24b97091a036f49f106f8f1b5ff90e385ad91577
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-08 21:44:27 +0530

    pattern updated

--- Pattern.py
+++ Pattern.py
@@ -317,3 +317,27 @@ for i in range(1,n+1):
 
 print() 
 
+# 1 1 1 1 1
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
+n = 5
+for i in range(1,n+1):
+    for j in range(n):
+        print(i,end=" ")
+    print()
+
+print()
+
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+n = 5
+for i in range(1,n+1):
+    for j in range(n):
+        print(j+1,end=" ")
+    print()
+print()
