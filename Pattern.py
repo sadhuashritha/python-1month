e1001ff1e709c9ef12f9b0ac8d9a202bcaa68284
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-08 21:43:32 +0530

    pattern updated

--- Pattern.py
+++ Pattern.py
@@ -303,3 +303,17 @@ for i in range(1,n+1):
             print(" ",end=" ")
     print()
 
+
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+n = 5
+for i in range(1,n+1):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+
+print() 
+
