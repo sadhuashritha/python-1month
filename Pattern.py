49f7db595b8aafc78de18f8742d62d741ad12e26
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-13 22:30:17 +0530

    updated

--- Pattern.py
+++ Pattern.py
@@ -455,16 +455,6 @@ for i in range(1,n+1):
     print()
 
 
-#      * * * * * 
-#     * * * * *
-#    * * * * *
-#   * * * * *
-#  * * * * *
-n = 5
-for i in range(n):
-    print(" " * (n-i-1),end=" ")
-    print("* " * n)
-
 # * * * * * 
 #   *   *
 #     *
