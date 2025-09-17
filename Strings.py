2d21c83a9fdee102f3206f1fa3bfa0590ad33409
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-17 22:47:54 +0530

    strings

--- Strings.py
+++ Strings.py
@@ -59,3 +59,22 @@ word1 = "".join(word1)
 word2 = "".join(word2)
 print(word1 == word2)
 
+# Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+# Given a balanced string s, split it into some number of substrings such that:
+# Each substring is balanced.
+# Return the maximum number of balanced strings you can obtain.
+# Example 1:
+# Input: s = "RLRRLLRLRL"
+# Output: 4
+
+R = 0
+L = 0
+res = 0
+for i in s:
+    if i == "L":
+        L += 1
+    else:
+        R += 1
+    if L == R:
+        res += 1
+print(res) 
