9450f90e14a79f6aca945cf9513f9a8cb579598a
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-19 22:35:41 +0530

    strings

--- Strings.py
+++ Strings.py
@@ -103,3 +103,18 @@ while i < len(s):
 print(res) 
 
 
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+# Return the merged string.
+word1 = "abc"
+word2 = "pqr"
+res = []
+l = min(len(word1), len(word2))
+
+for i in range(l):
+    res.append(word1[i])
+    res.append(word2[i])
+        
+res.append(word1[l:])
+res.append(word2[l:])
+
+print("".join(res))
