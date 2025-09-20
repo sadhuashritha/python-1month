c4f0c05b097453a53d1e3cb0784fa3588738812e
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-20 22:24:03 +0530

    strings

--- Strings.py
+++ Strings.py
@@ -119,13 +119,3 @@ res.append(word2[l:])
 
 print("".join(res))
 
-#  leet code : 2185 You are given an array of strings words and a string pref.
-# Return the number of strings in words that contain pref as a prefix.
-# A prefix of a string s is any leading contiguous substring of s.
-words = ["pay","attention","practice","attend"]
-pref = "at"
-res = 0
-for i in words:
-    if i.startswith(pref):
-        res += 1
-print(res)
