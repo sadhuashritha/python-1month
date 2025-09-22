1bb43788693c5aba7873a88820a1472dd9ca8a33
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-22 19:23:32 +0530

    List file added

--- List.py
+++ List.py
@@ -5,7 +5,7 @@ for i in arr:
     print(i,end=" ")
 print()
 
-# You are given a list that contains integers. You need to return the length of the list.
+# GFG   You are given a list that contains integers. You need to return the length of the list.
 arr = [54, 43, 2, 1, 5]
 # print(len(arr))
 count = 0
@@ -13,3 +13,15 @@ for i in arr:
     count += 1
 print(count)
 
+# GFG   You are given a list that contains integers. You need to return the sum of the list.
+arr = [54, 43, 2, 1, 5]
+sum = 0
+for i in arr:
+    sum += i
+print(sum)
+
+# GFG   You are given a list that contains integers. You need to decrement each element of the list by 1 and return the list.
+arr = [54, 43, 2, 1, 5]
+for i in range(len(arr)):
+    arr[i] -= 1
+print(arr)
