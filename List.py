d6d9ac182645633a7ffec270f32ef8fc11f60a81
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-09-22 19:07:30 +0530

    List file added

--- List.py
+++ List.py
@@ -1,5 +1,15 @@
-# You are given a list that contains integers. You need to print the elements of the list with a space between them.
+# GFG   You are given a list that contains integers. You need to print the elements of the list with a space between them.
 # Note: Do not add a new line at the end.
 arr = [54, 43, 2, 1, 5]
 for i in arr:
     print(i,end=" ")
+print()
+
+# You are given a list that contains integers. You need to return the length of the list.
+arr = [54, 43, 2, 1, 5]
+# print(len(arr))
+count = 0
+for i in arr:
+    count += 1
+print(count)
+
