d20f9161531465ac55de4b7dadf67d066054e0d6
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-24 22:42:27 +0530

    Update List.py

--- List.py
+++ List.py
@@ -49,3 +49,11 @@ sum = 0
 for i in range(1,len(nums)):
     nums[i] = nums[i-1] + nums[i]
 print(nums)
+
+# Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+# A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+nums = [0,2,1,5,3,4]
+ans = []
+for i in range(len(nums)):
+    ans.append(nums[nums[i]])
+print(ans)
