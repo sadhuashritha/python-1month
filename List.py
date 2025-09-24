d405bb4fda08d1ffe4280483cb0195caf4782182
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-24 22:43:34 +0530

    Update List.py

--- List.py
+++ List.py
@@ -64,3 +64,17 @@ sum = 0
 for i in range(1,len(nums)):
     nums[i] = nums[i-1] + nums[i]
 print(nums)
+
+# There is a programming language with only four operations and one variable X:
+# ++X and X++ increments the value of the variable X by 1.
+# --X and X-- decrements the value of the variable X by 1.
+# Initially, the value of X is 0.
+# Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+operations = ["--X","X++","X++"]
+X = 0
+for i in operations:
+    if i == "X++" or i == "++X":
+        X += 1
+    else:
+        X -= 1
+print(X)
