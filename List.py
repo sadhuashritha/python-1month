a52d53fed69d5f73b34b0be6a007ed497baed5a7
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-24 22:39:52 +0530

    Update List.py

--- List.py
+++ List.py
@@ -40,3 +40,12 @@ print(list)
 
 nums = [1,2,1]
 print(nums * 2)
+
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+
+nums = [1,2,3,4]
+sum = 0
+for i in range(1,len(nums)):
+    nums[i] = nums[i-1] + nums[i]
+print(nums)
