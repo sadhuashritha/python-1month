f5e8d9e18e6c43acefb1196e0f813294c6e1f465
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-25 11:36:41 +0530

    Update List.py

--- List.py
+++ List.py
@@ -92,3 +92,15 @@ for i in range(len(accounts)):
     if ans > max1:
         max1 = ans
 print(max1)
+
+# leetcode 1313 We are given a list nums of integers representing a list compressed with run-length encoding.
+# Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+# Return the decompressed list.
+
+nums = [1,2,3,4]
+ans = []
+for i in range(0,len(nums),2):
+    freq = nums[i]
+    val = nums[i + 1]
+    ans.extend([val] * freq)
+print(ans)
