63ff8cc615377c14a450996331fc298324425095
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-28 23:26:20 +0530

    Update List.py

--- List.py
+++ List.py
@@ -179,6 +179,21 @@ for i in sentences:
         count = res
 print(count)
 
+
+# 1720. Decode XORed Array
+# There is a hidden integer array arr that consists of n non-negative integers.
+# It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+# You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+# Return the original array arr. It can be proved that the answer exists and is unique.
+encoded = [1,2,3]
+first = 1
+ans = []
+ans.append(first)
+for i in range(len(encoded)):
+    ans.append(encoded[i] ^ ans[i])
+print(ans)
+
+
 # 1389. Create Target Array in the Given Order
 # Given two arrays of integers nums and index. Your task is to create target array under the following rules:
 # Initially target array is empty.
