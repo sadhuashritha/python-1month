08b1415e458ee0cea5a9449751937a8d7d3a3334
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-28 23:24:48 +0530

    Update List.py

--- List.py
+++ List.py
@@ -165,3 +165,16 @@ for i,j in zip(n1,n2):
     ans.append(i)
     ans.append(j)
 print(ans)
+
+# 2114. Maximum Number of Words Found in Sentences
+# A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+# You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+# Return the maximum number of words that appear in a single sentence.
+
+sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+count = 0
+for i in sentences:
+    res = len(i.split())
+    if res > count:
+        count = res
+print(count)
