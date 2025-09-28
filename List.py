59a7a9c345234bdefdf5121fb0ce67fd5ff6a201
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-28 23:24:17 +0530

    Update List.py

--- List.py
+++ List.py
@@ -149,3 +149,19 @@ maxpop = max(count)
 for i in range(1950,2050):
     if count[i] == maxpop:
 print(i)
+
+# 1470. Shuffle the Array
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+ ans = []
+# for i in range(n):
+#     ans.append(nums[i])
+#     ans.append(nums[i+n])
+# print(ans)
+n1 = nums[:n]
+n2 = nums[n:]
+for i,j in zip(n1,n2):
+    ans.append(i)
+    ans.append(j)
+print(ans)
