b8835b0d0b0155ba0437506d282704255e440887
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-09-30 21:38:23 +0530

    Update Day-6-Conditional.py

--- Day-6-Conditional.py
+++ Day-6-Conditional.py
@@ -297,17 +297,6 @@ num2 = int(b,2)
 print(bin(num1 + num2)[2:])
 
 
-# 69. Sqrt(x)
-# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
-# You must not use any built-in exponent function or operator.
-# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
-x = 4
-for i in range(1,x+1):
-    if i*i == x:
-        print(i)
-    elif i*i > x:
-        print(i-1)
-print(0)
 
 
 
