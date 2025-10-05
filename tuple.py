13f41383a8dcf5c2f0559b21ed477d6bdd10c770
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-05 18:51:04 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -110,3 +110,20 @@ for i in tup:
 print(min)
 print(max)
 
+# Write a program to check if two tuples (1,2,3) and (1,2,3) are identical.
+tup = (1,2,3)
+tup1 = (1,2,3)
+
+if tup == tup1:
+    print("Tuples are identical")
+else:
+    print("tuples are not identical")
+
+# You have a list of student marks as tuples [("John", 85), ("Alice", 92), ("Bob", 78)]. Sort them by marks.
+tup = [("John", 85), ("Alice", 92), ("Bob", 78)]
+ans = sorted(tup,key=lambda x : x[1])
+print(ans)
+
+
+
+
