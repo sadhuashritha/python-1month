6d0f45b82b27a91401efa00af4be462ed779144f
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-05 18:49:50 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -84,3 +84,11 @@ for i in tup:
 # Given a tuple of tuples ((1,2), (3,4), (5,6)), access the element 4.
 tup = ((1,2), (3,4), (5,6))
 print(tup[1][1])
+
+# Create a nested tuple for student details: ("John", (85, 90, 78)). Print the average marks.
+tup = ("John", (85, 90, 78))
+student = tup[0]
+marks = tup[1]
+avg = sum(marks)/ len(student)
+print("student" , student)
+print("total marks",avg)
