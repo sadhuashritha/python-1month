ccd3ec41060bb2e73cb217144ac565755cd3e6b6
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-07 23:05:15 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -125,3 +125,18 @@ ans = sorted(tup,key=lambda x : x[1])
 print(ans)
 
 
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans= []
+        for i in arr2:
+            for j in arr1:
+                if i == j:
+                    ans.append(i)
+
+        for j in sorted(arr1):
+            if j not in arr2:
+                ans.append(j)
+        return ans
+
+
+
