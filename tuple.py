4d89a06259dc06f049d9d92de4a258ed7008c8b2
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-08 23:35:29 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -138,27 +138,5 @@ class Solution:
                 ans.append(j)
         return ans
 
-# 1337. The K Weakest Rows in a Matrix
-# You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
-# A row i is weaker than a row j if one of the following is true:
-# The number of soldiers in row i is less than the number of soldiers in row j.
-# Both rows have the same number of soldiers and i < j.
-# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
-mat = [[1,1,0,0,0],
- [1,1,1,1,0],
- [1,0,0,0,0],
- [1,1,0,0,0],
- [1,1,1,1,1]], 
-k = 3
-ans = []
-for i in mat:
-    count = 0
-    for j in i :
-        if j == 1:
-            count += 1
-    ans.append(count)
-    
-index = list(range(len(mat)))
-index.sort(key = lambda x : ans[x])
-print(index[:k])
+
 
