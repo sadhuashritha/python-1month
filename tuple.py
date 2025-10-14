72522c41ba73c1cc124127b08f34dd5594c5fcec
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-14 23:27:26 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -290,3 +290,11 @@ for i in range(len(nums)):
             print(ans)
 print(False)
 
+# Given a tuple of strings ("apple", "banana", "grape", "kiwi"), print only the words that have more than 5 letters.
+tup = ["apples", "banana", "grape", "kiwi"]
+count = 0
+for i in tup:
+    if len(i) > 5:
+        print(i,end=" ")
+print("")
+
