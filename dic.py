7a2916c68e82ff2fb4bb610b25a15dbcacdaf2eb
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-21 22:28:15 +0530

    Update dic.py

--- dic.py
+++ dic.py
@@ -1,12 +1,4 @@
-# Given a list of strings containing names of students and 
-# another list containing marks of corresponding students. 
-# The task is to create a dictionary to store marks of students with their names (name will be unique).
-names = ["john", "ala", "ilia", "sudan", "mercy"]
-marks = [100, 200, 150, 80, 300]
-dict = {}
-for i,j in zip(names,marks):
-    dict[i] = j
-print(dict)
+
 
 # Create a dictionary named student with keys name, age, and grade and print it.
 student = {"name" : "ashritha" ,"age": 20, "grade": "A"}
