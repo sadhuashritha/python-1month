2b6b6b9793ef90a2931b7e053c6f823f14526556
sadhuashritha <152059046+sadhuashritha@users.noreply.github.com>
2025-10-21 22:29:59 +0530

    Update dic.py

--- dic.py
+++ dic.py
@@ -8,7 +8,9 @@ for i,j in zip(names,marks):
     dict[i] = j
 print(dict)
 
-
+# Create a dictionary named student with keys name, age, and grade and print it.
+student = {"name" : "ashritha" ,"age": 20, "grade": "A"}
+print(student)
 
 # How do you access the value of the key "age" from a dictionary named student?
 student = {"name" : "ashritha" ,"age": 20, "grade": "A"}
