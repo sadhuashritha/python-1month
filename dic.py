6c1effd00c661ef0ee7e60ec5ad59e836c8b68bc
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-10-23 23:21:47 +0530

    dict updated

--- dic.py
+++ dic.py
@@ -179,26 +179,26 @@ print(ans)
 
 # 387. First Unique Character in a String
 # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
-# s = "leetcode"
-# freq = {}
-# for i in s:
-#     if i in freq:
-#         freq[i] += 1
-#     else:
-#         freq[i] = 1
+s = "leetcode"
+freq = {}
+for i in s:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
 
-# for i,j in freq.items():
-#     if j == 1:
-#         print(s.index(i))
-#         break
-#     else:
-#         print(-1)
+for i,j in freq.items():
+    if j == 1:
+        print(s.index(i))
+        break
+    else:
+        print(-1)
 
 # or
 
-freq = Counter(s)
-for i,j in freq.items():
-    if j==1:
-       print(s.index(i))
-    else:
-        print(-1)
+# freq = Counter(s)
+# for i,j in freq.items():
+#     if j==1:
+#        print(s.index(i))
+#     else:
+#         print(-1)
