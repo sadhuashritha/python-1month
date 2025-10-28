8f151fbf74ef2a0b98027a0356f5c64a36c80581
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-10-28 23:19:03 +0530

    set updated

--- set.py
+++ set.py
@@ -6,3 +6,8 @@ print(set1)
 set1 ={1,2,3,4}
 set1.add(5)
 print(set1)
+
+# Write a program to add multiple elements to a set using the update() method.
+set1 ={1,2,3,4}
+set1.update([6,7])
+print(set1)
