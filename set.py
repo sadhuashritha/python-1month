10698941598e5fd7f942828e71aaa64266a348ef
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-10-28 23:18:10 +0530

    dict updated

---
+++ set.py
@@ -0,0 +1,13 @@
+# Write a Python program to create a set with some numbers and print it.
+set1 ={1,2,3,4}
+print(set1)
+
+# Write a program to add an element to a set using the add() method.
+set1 ={1,2,3,4}
+set1.add(5)
+print(set1)
+
+# Write a program to add multiple elements to a set using the update() method.
+set1 ={1,2,3,4}
+set1.update([6,7])
+print(set1)
