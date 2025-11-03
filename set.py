bd21f2fd1dabff5cf8407069d75a57238ed9eae1
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-11-03 23:13:15 +0530

    set updated

--- set.py
+++ set.py
@@ -143,5 +143,12 @@ if set1 == set3:
 else:
     print("Both are not equal")
 
-
+# 268. Missing Number
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+nums = [3,0,1]
+n = len(nums)
+ans = n * (n + 1) // 2
+ans1 = sum(nums)
+ans2 = ans - ans1
+print(ans2)
 
