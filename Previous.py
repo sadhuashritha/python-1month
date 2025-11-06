cbc00bc64c1280478592e4664bfe9f57498f2a32
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-11-06 20:32:10 +0530

    previous topics file

--- Previous.py
+++ Previous.py
@@ -1,11 +1,11 @@
-# Take the user’s name and age and print: "Hello <name>, you are <age> years old."
+# 1.Take the user’s name and age and print: "Hello <name>, you are <age> years old."
 name = "Ashritha"
 age = 20
 print("Hello",name,"you are",age,"years old")
 print("%s is %d years old." % (name, age))
 print("Hello %s you are %d years old"% (name,age))
 
-# Take two integers and print their sum, difference, multiplication, and division.
+# 2.Take two integers and print their sum, difference, multiplication, and division.
 n = 10
 m = 20
 sum = n + m
@@ -14,7 +14,7 @@ mul = n * m
 div = n / m
 print("sum: ",sum,"difference: ",diff,"multiplication: ",mul,"division: ",div)
 
-# Input three numbers and print the largest.
+# 3.Input three numbers and print the largest.
 a = 10
 b = 100
 c = 30
@@ -25,7 +25,7 @@ elif b > a and b > c :
 else:
     print("Largest number is c: ",c) 
 
-# Input a number and check whether it is positive, negative, or zero.
+#4. Input a number and check whether it is positive, negative, or zero.
 x = 0
 if x > 0:
     print(x, " is positive number")
@@ -33,3 +33,9 @@ elif x < 0:
     print(x," is negative number")
 else:
     print(x," is zero")
+
+# 5. Input a floating-point number and round it to two decimals.
+n = float(input("Enter a decimal number: "))
+print(n.__round__(2))
+print(round(n,2))
+
