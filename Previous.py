80240a74c29e8b364cf5e465949d8fa262dc149b
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-11-06 20:18:30 +0530

    previous topics file

---
+++ Previous.py
@@ -0,0 +1,15 @@
+# Take the user’s name and age and print: "Hello <name>, you are <age> years old."
+name = "Ashritha"
+age = 20
+print("Hello",name,"you are",age,"years old")
+print("%s is %d years old." % (name, age))
+print("Hello %s you are %d years old"% (name,age))
+
+# Take two integers and print their sum, difference, multiplication, and division.
+n = 10
+m = 20
+sum = n + m
+diff = n - m
+mul = n * m
+div = n / m
+print("sum: ",sum,"difference: ",diff,"multiplication: ",mul,"division: ",div)
