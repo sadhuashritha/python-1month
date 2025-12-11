428000394565f8e66833708bf7e7dde51a1c38ea
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-12-11 19:38:30 +0530

    Updated File

--- Practice.py
+++ Practice.py
@@ -19,3 +19,22 @@ print(string)
 # 4.Write a program to take your name as input and print "Hello, <name>!".
 Name  = "Ashritha"
 print("Hello",name,"!")
+
+# 5.Write a program that asks the user for two numbers and prints their sum.
+n = 10
+m = 20
+print(n + m)
+
+# 6.Write a program that asks the user for their age and prints whether they are a minor or an adult (just print the message, no conditions yet).
+age =30
+print("You are a minor or an adult")
+
+# 7.Write a program that uses input() to read a string and then prints its length.
+s = "Ashritha"
+print(len(s))
+
+# 8.Write a program to read a line from the user and print it in this format:
+# You entered: <user_input>.
+# n = int(input("Enter Your Name"))
+n = "Ashritha"
+print("You entered: " , name)
