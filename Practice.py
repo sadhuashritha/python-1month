4b17f267bc3e298a3236e8fc9df5e2deb3f7c5bb
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-12-11 19:13:14 +0530

    Added File

---
+++ Practice.py
@@ -0,0 +1,21 @@
+# Python Introduction + Input/Output
+# 1.Write a program to print "Hello, World!".
+print("Hello World")
+
+# 2.Write a program to print your name, age, and city on separate lines.
+name = "Ashritha"
+age = 20
+city = "Medak"
+print(name)
+print(age)
+print(city)
+
+# 3.Write a program that prints a multi-line string using triple quotes.
+string = """program that prints a 
+multi-line string using 
+triple quotes."""
+print(string)
+
+# 4.Write a program to take your name as input and print "Hello, <name>!".
+Name  = "Ashritha"
+print("Hello",name,"!")
