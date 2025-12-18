87c0bc62ff5671397534851e5c17a2a500fd4eba
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-12-18 16:18:30 +0530

    updated

--- Practice.py
+++ Practice.py
@@ -615,9 +615,6 @@ if b == "a"or b=="e"or b=="i"or b=="o"or b=="u":
 else:
     print("Consonant")
 print()
-# or
-b= "a"
-if b in "aeiou":
-    print("Vowel")
-else:
-    print("Consonant")
+
+
+# 78.Write a program that checks if a character is uppercase, lowercase, digit, or special character.
