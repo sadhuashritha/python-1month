a70d0f8e82297a300a4d8840b151e01b893358b3
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2025-12-27 20:36:14 +0530

    updated

--- Practice.py
+++ Practice.py
@@ -686,4 +686,7 @@ else:
     n = a // b 
     print(c)
 
-
+# 84. Write a program that reads two angles of a triangle and checks if the third angle is valid (sum must be 180).
+a = 10
+b = 100
+c = 70
