5f7b0432b4aa259b7cf1e84e54624af2518d7cb1
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-01 22:29:57 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -8,4 +8,24 @@ class Solution:
                     count += 1
         return count
                     
+# 1051. Height Checker
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        # expected = []
+        # for i in range(len(heights)):
+        #     expected.append(heights[i])
+
+
+        # for i in range(len(expected)):
+        #     for j in range(len(expected)-1):
+        #         if expected[j] > expected[j+1]:
+        #             expected[j],expected[j+1] = expected[j+1],expected[j]
+        expected = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count += 1
+    
+        return count
+
 # 
