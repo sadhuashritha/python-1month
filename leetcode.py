ca4f202a5a983dfd4a00eb6f4f49c0f83b10acb4
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-08 22:07:49 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -163,4 +163,18 @@ class Solution:
         s = ''.join(sorted(s))
         t = ''.join(sorted(t))
         return s==t
+    
+# 49. Group Anagrams
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 0:
+            return [[""]]
+        ans = {}
+        for i in range(len(strs)):
+            res = ''.join(sorted(strs[i]))
+            if res in ans:
+                ans[res].append(strs[i])
+            else:
+                ans[res] = [strs[i]]
 
+        return list(ans.values())
