59e0da6afe2cb0dec78f9e059454b53819510cde
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-10 22:16:01 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -178,3 +178,19 @@ class Solution:
                 ans[res] = [strs[i]]
 
         return list(ans.values())
+    
+# 169. Majority Element
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[nums[i]] = 1
+            else:
+                d[nums[i]]+=1
+        
+        # return max(d, key=d.get)
+        n = len(nums)
+        for key,value in d.items():
+            if value > (n/2):
+                return key
