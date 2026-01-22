a544385b043d798f9b564eb54437d613c11f32c8
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-22 22:58:20 +0530

    updated

--- practice1.py
+++ practice1.py
@@ -120,6 +120,22 @@ for i in range(len(a)):
         d[a[i]] += 1
 print(d)
 
+# Pair sum
+arr = [2, 7, 11, 15]
+target = 9
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i] + arr[j] == target:
+            print(i,j)
+            found = True
+    if found:
+        break
+if not found:
+    print("Not found")
+
+    
+
+    
 
 
 
