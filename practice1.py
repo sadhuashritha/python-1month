bc57bc18be40ff0bc72aa2b61fc7f0b5d483b54d
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-26 21:09:52 +0530

    updated

--- practice1.py
+++ practice1.py
@@ -161,15 +161,6 @@ for i in range(2000,3201):
 print()
 print()
 
-# Question: Write a program which can compute the factorial of a given numbers.
-#  The results should be printed in a comma-separated sequence on a single line. 
-# Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
 
-num = 8
-sum = 1
-for i in range(1,num+1):
-    sum *= i
-
-print(sum)
 
 
