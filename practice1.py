36fb9b93ad9d38c6fd4c3165758b1ade4ac9a2f2
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-01-27 20:52:30 +0530

    updated

--- practice1.py
+++ practice1.py
@@ -172,19 +172,6 @@ for i in range(1,num+1):
 
 print(sum)
 
-# Question: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). 
-# and then the program should print the dictionary. 
-# Suppose the following input is supplied to the program: 8 Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
-# Hints: In case of input data being supplied to the question, it should be assumed to be a console input. Consider use dict()
-
-a = ["tan","can","eat","ate","man","bat"]
-d = {}
-for i in range(len(a)):
-    ans = "".join(sorted(a[i]))
-    if ans not in d:    
-        d[ans].append(a[i])
-    else:
-        d[ans] = a[i]
   
 
 
