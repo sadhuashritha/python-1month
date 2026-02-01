e1f3f69677c6b968adb28e4f100ac169659fb972
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-01 23:15:07 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -328,3 +328,13 @@ class Solution:
             nums[i] = res[i]
         return len(res)
         
+# 2824. Count Pairs Whose Sum is Less than Target
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] < target :
+                    count += 1      
+        return count
+
