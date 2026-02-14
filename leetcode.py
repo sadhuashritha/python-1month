841d20d7e4c77e208eed1da9e6956bd247302368
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-14 23:43:03 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -394,3 +394,25 @@ class Solution:
                 left += 1
             else:
                 right -= 1
+
+# 2129. Capitalize the Title
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        words = title.split()
+        for i in range(len(words)):
+            if len(words[i]) <= 2:
+                words[i] = words[i].lower()
+            else:
+                words[i] = words[i][0].upper() + words[i][1:].lower()
+        return " ".join(words)
+    
+# 520. Detect Capital
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        words = title.split()
+        for i in range(len(words)):
+            if len(words[i]) <= 2:
+                words[i] = words[i].lower()
+            else:
+                words[i] = words[i][0].upper() + words[i][1:].lower()
+        return " ".join(words)
