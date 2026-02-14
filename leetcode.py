62ca30fee8034ec5467d231330785cccd1d7d23b
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-14 23:43:24 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -406,13 +406,3 @@ class Solution:
                 words[i] = words[i][0].upper() + words[i][1:].lower()
         return " ".join(words)
     
-# 520. Detect Capital
-class Solution:
-    def capitalizeTitle(self, title: str) -> str:
-        words = title.split()
-        for i in range(len(words)):
-            if len(words[i]) <= 2:
-                words[i] = words[i].lower()
-            else:
-                words[i] = words[i][0].upper() + words[i][1:].lower()
-        return " ".join(words)
