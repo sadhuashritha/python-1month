3f1ea0e51de10084c56c32dab51dd8f901e5067c
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-17 23:08:57 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -437,3 +437,11 @@ class Solution:
         bit = bits[::-1]
         return int(bit,2)
          
+# Sum Multiples
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        sum = 0
+        for i in range(1,n+1):
+            if i % 3 == 0 or i % 5 == 0 or  i % 7 == 0:
+                sum += i
+        return sum
