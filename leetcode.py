21aecd02a02eb8226848f2831ffdf9cbe525f041
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-19 22:50:27 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -537,3 +537,19 @@ class Solution:
                 count += 1
         return count
     
+# Reverse Only Letters
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        p1 = 0
+        p2 = len(s) - 1
+        while p1 < p2:
+            if not s[p1].isalpha():
+                p1+=1
+            elif not s[p2].isalpha():
+                p2-=1
+            else:
+                s[p1],s[p2] = s[p2],s[p1]
+                p1 += 1
+                p2 -=1
+        return "".join(s)
