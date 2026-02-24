0935c5efd17a945d721d18d183c963d51c1d7bbf
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-24 22:52:49 +0530

    Updated problems

--- leetcode.py
+++ leetcode.py
@@ -600,3 +600,12 @@ class Solution:
             if i not in nums:
                 return i
                 break
+
+# 1832. Check if the Sentence Is Pangram
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            if i not in sentence:
+                return False
+        return True
+        
