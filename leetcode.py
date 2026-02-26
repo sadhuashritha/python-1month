29abf7d50aceacd00b3673e4b5c26bcb9e047598
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-02-26 23:26:42 +0530

    Updated problems

--- leetcode.py
+++ leetcode.py
@@ -616,9 +616,4 @@ class Solution:
         s2 = s[k:len(s)]
         return s1 + s2
 
-# 1464. Maximum Product of Two Elements in an Array
-class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        nums.sort(reverse= True)
-        ans = (nums[0]-1)*(nums[1]-1)
-        return ans
+
