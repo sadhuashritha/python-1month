4cf2031083aebd99b5d01a12d2f630aabb365e0e
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-03-09 22:14:34 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -691,3 +691,9 @@ class Solution:
         # return num
         return num.rstrip('0')
 
+# Find the Difference of Two Arrays
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return[list(set1 - set2), list(set2 - set1)]
