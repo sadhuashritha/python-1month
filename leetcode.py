e6a9b310b988de6b1c0c09cbe55fd21dab86dcf4
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-03-12 23:14:37 +0530

    updated

--- leetcode.py
+++ leetcode.py
@@ -760,3 +760,12 @@ class Solution:
         for i in nums:
             if i != mini and i != maxi:
                 return i
+            
+# 414. Third Maximum Number
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort(reverse = True)
+        if len(nums) < 3:
+            return nums[0]
+        return nums[2]
